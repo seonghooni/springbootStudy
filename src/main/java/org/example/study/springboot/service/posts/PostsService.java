bc7a820ffd515cc8221833ee3e14002b53c06229
seonghooni <lsh3717@naver.com>
2022-12-27 19:15:13 +0900

    4장

---
+++ src/main/java/org/example/study/springboot/service/posts/PostsService.java
@@ -0,0 +1,58 @@
+package org.example.study.springboot.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.example.study.springboot.domain.posts.Posts;
+import org.example.study.springboot.domain.posts.PostsRepository;
+import org.example.study.springboot.web.dto.PostsListResponseDto;
+import org.example.study.springboot.web.dto.PostsResponseDto;
+import org.example.study.springboot.web.dto.PostsSaveRequestDto;
+import org.example.study.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
